3f8f3b07b1f381b18550e4e54900cabf52ecdb32
ambullon <antoniomorobullon@gmail.com>
2018-09-26 16:32:09 +0200

    Until part 8

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -48,6 +48,30 @@ export function reducer(state = initialState, action: PizzaAction): PizzaState {
         loaded: false
       };
     }
+
+    case fromPizzas.UPDATE_PIZZA_SUCCESS:
+    case fromPizzas.CREATE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const entities = {
+        ...state.entities,
+        [pizza.id]: pizza
+      };
+
+      return {
+        ...state,
+        entities
+      }
+    }
+
+    case fromPizzas.REMOVE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const { [pizza.id]: removed, ...entities } = state.entities;
+
+      return {
+        ...state,
+        entities
+      }
+    }
   }
   return state;
 }
